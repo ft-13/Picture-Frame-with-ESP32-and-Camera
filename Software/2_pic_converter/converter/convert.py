06a3b8ab2b8d9c9b7da7519b56d3a725c5bf5041
Frank Khelfa <frank.khelfa@zal.aero>
2024-01-04 19:26:56 +0100

    modified

--- Software/2_pic_converter/converter/convert.py
+++ Software/2_pic_converter/converter/convert.py
@@ -6,43 +6,35 @@ import shutil
 
 
 ext = [".jpg", ".JPG", ".jpeg", ".JPEG"]
+size = "1024x768"
 
 # folder path
 dir_path = r'./'
 
-# list to store files
+# list of generated temp files for removing
 temp_files = []
 # Iterate directory
 for file in os.listdir(dir_path):
-    # check only text files
+    # check only jpg files
     if file.endswith(tuple(ext)):
         output_gray = os.path.splitext(os.path.basename(file))[0] + '_gray' + os.path.splitext(os.path.basename(file))[1]
         
-        process = subprocess.Popen('magick convert' + ' ' + file + ' ' + '-colorspace Gray' + ' ' + output_gray, stdout=subprocess.PIPE, creationflags=0x08000000)
-        process.wait()
+        gray_convert = subprocess.Popen('magick convert' + ' ' + file + ' ' + '-colorspace Gray' + ' ' + output_gray, stdout=subprocess.PIPE, creationflags=0x08000000)
+        gray_convert.wait()
         temp_files.append(output_gray)
         
         input_gray = output_gray
         output_resized = os.path.splitext(os.path.basename(input_gray))[0] + '_resized' + os.path.splitext(os.path.basename(input_gray))[1]
         
-        process = subprocess.Popen('magick convert' + ' ' + input_gray + ' ' + '-resize 1024x768!' + ' ' + output_resized, stdout=subprocess.PIPE, creationflags=0x08000000)
-        process.wait()
+        resize = subprocess.Popen('magick convert' + ' ' + input_gray + ' ' + '-resize' + ' ' + size + '!' + ' ' + output_resized, stdout=subprocess.PIPE, creationflags=0x08000000)
+        resize.wait()
         temp_files.append(output_resized)
         
         input_resized = output_resized
         output_raw = os.path.splitext(os.path.basename(file))[0] + '.gray'
         
-        process = subprocess.Popen('magick convert' + ' ' + input_resized + ' ' + '-depth 8' + ' ' + output_raw, stdout=subprocess.PIPE, creationflags=0x08000000)
-        process.wait()
-
-
-# getting length of list
-length = len(temp_files)
-  
-# Iterating the index
-# same as 'for i in range(len(list))'
-#for i in range(length):
-    #os.remove(temp_files[i])
+        raw_bytes = subprocess.Popen('magick convert' + ' ' + input_resized + ' ' + '-depth 8' + ' ' + output_raw, stdout=subprocess.PIPE, creationflags=0x08000000)
+        raw_bytes.wait()
 
 
 #move created .gray files to output folder and rename extension to .txt
@@ -62,6 +54,7 @@ for file in os.listdir(dir_path):
             shutil.move(src, dest)
 
 
-
-
+# comment out if you need the generated temp files
+for i in range(len(temp_files)):
+    os.remove(temp_files[i])
 
